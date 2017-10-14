af0903f91eccdee88191be9626e3adf66a941f27
Karthik Kumar Challa <challakarthik@Challas-MacBook-Pro.local>
2017-10-14 22:51:50 +0530

    Experimented signature, But can still perform better.

--- TPL-Sales/PaymentViewController.swift
+++ TPL-Sales/PaymentViewController.swift
@@ -27,11 +27,24 @@ class PaymentViewController: UIViewController {
     @IBOutlet weak var previouslyPaid: UILabel!
     @IBOutlet weak var employeesList: UITextField!
     @IBOutlet weak var authorize: UIButton!
-    @IBOutlet weak var signature: UITextView!
+    @IBOutlet weak var signatureView: UIView!
+    @IBOutlet weak var signatureToolBar: UIToolbar!
+    @IBOutlet weak var signatureMainImage: UIImageView!
+    @IBOutlet weak var signatureTempImage: UIImageView!
+    @IBOutlet weak var signatureReset: UIBarButtonItem!
+    @IBOutlet weak var signatureTitle: UIBarButtonItem!
+    @IBOutlet weak var signatureDone: UIBarButtonItem!
     @IBOutlet weak var authorizationDesc: UITextView!
     
     // MARK: Internal variables
     var isAuthorized = false
+    var lastPoint = CGPoint.zero
+    var red: CGFloat = 0.0
+    var green: CGFloat = 0.0
+    var blue: CGFloat = 0.0
+    var brushWidth: CGFloat = 3.0
+    var opacity: CGFloat = 1.0
+    var swiped = false
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -39,6 +52,60 @@ class PaymentViewController: UIViewController {
         employeesList.addRightView(imageName: "downarrow", widthPadding: 20)
 
         performCreditCard(creditCard)
+    }
+    
+    // MARK: Drawing controller delegates
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        swiped = false
+        if let touch = touches.first {
+            lastPoint = touch.location(in: signatureView)
+        }
+    }
+    
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+        swiped = true
+        if let touch = touches.first {
+            let currentPoint = touch.location(in: signatureView)
+            drawLineFrom(fromPoint: lastPoint, toPoint: currentPoint)
+            lastPoint = currentPoint
+        }
+    }
+    
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+        if !swiped {
+            drawLineFrom(fromPoint: lastPoint, toPoint: lastPoint)
+        }
+        
+        // Merge tempImageView into mainImageView
+        UIGraphicsBeginImageContext(signatureMainImage.frame.size)
+        signatureMainImage.image?.draw(in: CGRect(x: 0, y: 0, width: signatureView.frame.size.width, height: signatureView.frame.size.height), blendMode: .normal, alpha: 1.0)
+        signatureTempImage.image?.draw(in: CGRect(x: 0, y: 0, width: signatureView.frame.size.width, height: signatureView.frame.size.height), blendMode: .normal, alpha: opacity)
+        signatureTempImage.image = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        
+        signatureTempImage.image = nil
+    }
+    
+    func drawLineFrom(fromPoint: CGPoint, toPoint: CGPoint) {
+        
+        UIGraphicsBeginImageContext(signatureView.frame.size)
+        let context = UIGraphicsGetCurrentContext()
+        signatureTempImage.image?.draw(in: CGRect(x: 0, y: 0, width: signatureView.frame.size.width, height: signatureView.frame.size.height))
+        
+        context?.move(to: fromPoint)
+        context?.addLine(to: toPoint)
+        
+        context?.setLineCap(.round)
+        context?.setLineWidth(brushWidth)
+        context?.setStrokeColor(red: red, green: green, blue: blue, alpha: 1.0)
+        context?.setBlendMode(.normal)
+        
+        context?.strokePath()
+        
+        signatureTempImage.image = UIGraphicsGetImageFromCurrentImageContext()
+        signatureTempImage.alpha = opacity
+        UIGraphicsEndImageContext()
     }
     
     // MARK: Actions
@@ -125,6 +192,13 @@ class PaymentViewController: UIViewController {
         chequePayment.setImage(UIImage(named: "cheque-inactive"), for: .normal)
         chequePayment.setTitleColor(ColorConstants.textGray, for: .normal)
         chequePayment.layer.borderColor = UIColor.lightGray.cgColor
+    }
+    
+    @IBAction func performSignatureReset(_ sender: UIBarButtonItem) {
+        
+    }
+    
+    @IBAction func performSignatureDone(_ sender: UIBarButtonItem) {
     }
 
 }
